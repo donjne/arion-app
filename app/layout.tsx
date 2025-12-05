aa4023c32cfdce0fa3d43ead182f83b35b5418d4
donjne <davidjrn247@gmail.com>
2025-12-05 02:18:00 +0100

    status: updating config.........

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${cinzel.variable}`}>
-      <body className="font-body">
+      <body>
         <Providers>
           {children}
         </Providers>
