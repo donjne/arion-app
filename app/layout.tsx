1e90ec105bbcc4250582692fdc32e83625d463f8
donjne <davidjrn247@gmail.com>
2025-12-05 02:10:00 +0100

    status: updating tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Cinzel } from "next/font/google";
-import "./globals.css";
 import { Providers } from "./providers";
 
 const inter = Inter({ 
